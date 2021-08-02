52d64cb6b9f89ab5945c6b73f4a966c003f53c1b
Kevin Kunkel <kunkelkevin@yahoo.com>
2021-08-01 21:26:44 -0500

    add thought

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,21 @@
 const { User, Thought } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("thoughts")
+          .populate("friends");
+
+        return userData;
+      }
+      throw new AuthenticationError("Not logged in");
+    },
+
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Thought.find(params).sort({ createAt: -1 });
@@ -30,8 +43,9 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
+      const token = signToken(user);
 
-      return user;
+      return { token, user };
     },
 
     login: async (parent, { email, password }) => {
@@ -47,7 +61,26 @@ const resolvers = {
         throw new AuthenticationError("Incorrect credentials");
       }
 
-      return user;
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addThought: async (parent, args, context) => {
+      if (context.user) {
+        const thought = await Thought.create({
+          ...args,
+          username: context.user.username,
+        });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { thoughts: thought._id } },
+          { new: true }
+        );
+
+        return thought;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 };

3ae1600cdca8a35987f1bccf62f677e7cfd9c517
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-08-04 09:22:19 -0500

    conditional rendering for logged in user

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,18 @@ const httpLink = createHttpLink({
   uri: '/graphql'
 });
 
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
